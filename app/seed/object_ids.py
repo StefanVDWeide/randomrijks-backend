9090632c4fd9d7bf1cd6a1c2fcca406242a6e4af
Stefan <stefan.vander.weide@hotmail.com>
2021-03-05 00:10:14 +0100

    Possible memory fix based on direct database add

--- app/seed/object_ids.py
+++ app/seed/object_ids.py
@@ -12,25 +12,26 @@ def insert_object_ids(db):
     basedir = os.path.abspath(os.path.dirname(__file__))
     object_csv_file = os.path.join(basedir, "data/202001-rma-csv-collection.csv")
 
-    object_ids = []
+    # object_ids = []
 
     # Utilize Panda's chunk size functionality to read the csv file in smaller batches
     # to circumvent the servers memory constraints
-    chunk_size = 1000
-    count = 1
-    for chunk in pd.read_csv(object_csv_file, chunksize=chunk_size):
-        for row in chunk.iterrows():
-            if row[1]["objectImage"]:
-                object_ids.append(Object(number=row[1]["objectInventoryNumber"]))
+    # chunk_size = 1000
+    # count = 1
+    # for chunk in pd.read_csv(object_csv_file, chunksize=chunk_size):
+    #     for row in chunk.iterrows():
+    #         if row[1]["objectImage"]:
+    #             object_ids.append(Object(number=row[1]["objectInventoryNumber"]))
 
-    # with open(object_csv_file, "r") as f:
-    #     r = reader(f)
-    #     for row in r:
-    #         # Check if the object has an image availble
-    #         if row[1]:
-    #             object_ids.append(Object(number=row[0]))
+    with open(object_csv_file, "r") as f:
+        r = reader(f)
+        for row in r:
+            # Check if the object has an image availble
+            if row[1]:
+                # object_ids.append(Object(number=row[0]))
+                db.session.add(Object(number=row[0]))
 
-    db.session.bulk_save_objects(object_ids)
+    # db.session.bulk_save_objects(object_ids)
     db.session.commit()
 
     return "Added object IDs to database"
