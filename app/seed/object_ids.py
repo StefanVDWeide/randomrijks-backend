f73135a62819ae695c1d13e8854854d9a77573a5
Stefan <stefan.vander.weide@hotmail.com>
2021-03-05 00:00:28 +0100

    Added possible seed database memory fix

--- app/seed/object_ids.py
+++ app/seed/object_ids.py
@@ -1,4 +1,5 @@
 from csv import reader
+import pandas as pd
 import os
 
 
@@ -13,12 +14,21 @@ def insert_object_ids(db):
 
     object_ids = []
 
-    with open(object_csv_file, "r") as f:
-        r = reader(f)
-        for row in r:
-            # Check if the object has an image availble
-            if row[1]:
-                object_ids.append(Object(number=row[0]))
+    # Utilize Panda's chunk size functionality to read the csv file in smaller batches
+    # to circumvent the servers memory constraints
+    chunk_size = 1000
+    count = 1
+    for chunk in pd.read_csv(object_csv_file, chunksize=chunk_size):
+        for row in chunk.iterrows():
+            if row[1]["objectImage"]:
+                object_ids.append(Object(number=row[1]["objectInventoryNumber"]))
+
+    # with open(object_csv_file, "r") as f:
+    #     r = reader(f)
+    #     for row in r:
+    #         # Check if the object has an image availble
+    #         if row[1]:
+    #             object_ids.append(Object(number=row[0]))
 
     db.session.bulk_save_objects(object_ids)
     db.session.commit()
